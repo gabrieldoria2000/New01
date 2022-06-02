6b6e96ee08d0c356998cc2033dcc25721095794a
gabrieldoria2000 <gabrieldoria2000@gmail.com>
2022-06-02 14:35:08 -0300

    primeiro commit

---
+++ New01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace New01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
